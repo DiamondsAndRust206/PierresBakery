f51b48d5f7f5c5b3202f7e8a08d89543d20da23d
Timothy RYAN <ryaninlux@gmail.com>
2022-07-02 12:16:53 -0700

    Finish testing my UI in programs.cs. If else statment works properly

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,10 +6,12 @@ public class Program
 {
   public static void Main()
   {
-    Console.WriteLine("----------Welcome To Pierre's Bakery!----------");
-    Console.WriteLine("Our Bread Prices are: $5 for each loaf, or every third loaf of bread is free!!!");
-    Console.WriteLine("Our Pastry Prices are: $2 for each || If you get between three and five pastries you get a $1 off || Every half dozen is $2 off!!!");
-    Console.WriteLine("-------------------------------------------------------------------------------");
+    Console.WriteLine("");
+    Console.WriteLine("             ----------WELCOME TO PIERRE'S BAKERY!----------");
+    Console.WriteLine("");
+    Console.WriteLine("BREAD PRICES : $5 for each loaf, or every third loaf of bread is free!!!");
+    Console.WriteLine("PASTRY PRICES : $2 for each || If you get between three and five pastries you get a $1 off || Every half dozen is $2 off!!!");
+    Console.WriteLine("=========================================================================");
 
     Console.WriteLine("How many loaves of bread would you like?");
     string breadInput = Console.ReadLine();
@@ -17,8 +19,45 @@ public class Program
     newBread.BreadSales();
     newBread.BreadDiscount();
     Console.WriteLine($"You ordered {breadInput} loaves of bread");
-    int totalPrice = newBread.BreadSalesTotal;
-    Console.WriteLine(totalPrice);
+    Console.WriteLine("-----------------------------------------------------");
 
+    Console.WriteLine("");
+    Console.WriteLine("How many pastries would you like?");
+    string pastryInput = Console.ReadLine();
+    Pastry newPastry = new Pastry(pastryInput);
+    newPastry.PastrySales();
+    newPastry.PastryDiscount();
+    Console.WriteLine($"You ordered {pastryInput} pastries");
+    Console.WriteLine("------------------------------------------------------");
+
+    Console.WriteLine("");
+    int totalPrice = newBread.BreadSalesTotal + newPastry.PastrySalesTotal;
+    Console.WriteLine("Your Total is: " + "$" + totalPrice);
+    Console.WriteLine("Would you like to complete your purchase? (Yes/No)");
+    string purchase = Console.ReadLine();
+    if (purchase.ToLower() == "yes")
+    {
+      Console.WriteLine("");
+      Console.WriteLine("Your order is complete. Thank you for your business!!");
+      Console.WriteLine("Press enter to return to main menu.");
+      string toTop = Console.ReadLine();
+      Main();
+    }
+    else if (purchase.ToLower() == "no")
+    {
+      Console.WriteLine("");
+      Console.WriteLine("Your order is canceled. You will not be charged.");
+      Console.WriteLine("Press enter to return to main menu.");
+      string toTop = Console.ReadLine();
+      Main();
+    }
+    else
+    {
+      Console.WriteLine("");
+      Console.WriteLine("Incorrect input. Press enter to start your order over.");
+      string toTop = Console.ReadLine();
+      Main();
+    }
+  
   }
 }
