862ac85d13109e498a8461fb3c968e4af50445a8
Timothy RYAN <ryaninlux@gmail.com>
2022-07-01 19:36:00 -0700

    Allow user to at the number of bread they want and return the correct price with discount

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,6 +8,17 @@ public class Program
   {
     Console.WriteLine("----------Welcome To Pierre's Bakery!----------");
     Console.WriteLine("Our Bread Prices are: $5 for each loaf, or every third loaf of bread is free!!!");
-    Console.WriteLine("Our Pastry Prices are: $2 for each || If you get between three and five pastries you get a $1 off || Half a dozen is only $10!!!");
+    Console.WriteLine("Our Pastry Prices are: $2 for each || If you get between three and five pastries you get a $1 off || Every half dozen is $2 off!!!");
+    Console.WriteLine("-------------------------------------------------------------------------------");
+
+    Console.WriteLine("How many loaves of bread would you like?");
+    string breadInput = Console.ReadLine();
+    Bread newBread = new Bread(breadInput);
+    newBread.BreadSales();
+    newBread.BreadDiscount();
+    Console.WriteLine($"You ordered {breadInput} loaves of bread");
+    int totalPrice = newBread.BreadSalesTotal;
+    Console.WriteLine(totalPrice);
+
   }
 }
